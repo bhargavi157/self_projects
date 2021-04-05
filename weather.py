bef4a70a61d07f05109f5829e97ed765b3af9ba7
bhargavi157 <63589533+bhargavi157@users.noreply.github.com>
2021-04-05 23:12:27 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -72,7 +72,7 @@ x = response.json()
 # Check the value of "cod" key is equal to
 # "404", means city is found otherwise,
 # city is not found
-if x["cod"] != "404" or none:
+if x["cod"] != "404" or " ":
   
     # store the value of "main"
     # key in variable y
